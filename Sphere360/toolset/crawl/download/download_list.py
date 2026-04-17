6c1a8c529e74c49c2ab2da781b7152c481d37a4d
Ilpo Viertola <ilpo.viertola@gmail.com>
2026-04-17 15:29:45 +0300

    add cookies to download script args

--- Sphere360/toolset/crawl/download/download_list.py
+++ Sphere360/toolset/crawl/download/download_list.py
@@ -1,27 +1,27 @@
 import sys
+
 sys.path.append("..")
 import core
 from core import download
 
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-i", "--input-csv", type=str, required=True
-    )
+    parser.add_argument("-i", "--input-csv", type=str, required=True)
     parser.add_argument(
         "-o", "--output-dir", type=str, required=True, help="Output directory"
     )
+    parser.add_argument("-j", "--jobs", type=int, default=8, help="Number of jobs")
     parser.add_argument(
-        "-j", "--jobs", type=int, default=8, help="Number of jobs"
+        "-c", "--cookie", type=str, default=None, help="Path to cookies.txt file"
     )
     args = parser.parse_args()
     input_csv = args.input_csv
     output_dir = args.output_dir
     jobs = args.jobs
+    cookie = args.cookie
 
     download.download_list.download_list_360(
-        input_file = input_csv,
-        output_folder = output_dir,
-        jobs = jobs
+        input_file=input_csv, output_folder=output_dir, jobs=jobs, cookie=cookie
     )
